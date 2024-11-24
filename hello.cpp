701ee0dcac1a29caa145c1486d94238b72a96994
Semi Eren Gokyildiz <gokyildizsemieren@gmail.com>
2024-11-24 21:32:33 +0300

    Compiler commit

--- hello.cpp
+++ hello.cpp
@@ -1,11 +1,19 @@
+/*
+hello.cpp
+Prints name, home and hobby
+by Eren
+*/
+
 #include <cstdio>
 
 int main()
 {
+    //Prints "Hello world"
     printf("Hello world\n");
-
-    //Challenge
-
+    //Prints my name
+    printf("My name is Eren\n");
+    //Prints my home
     printf("My home is in Istanbul\n");
-    printf("My hobies is playing a computer\n");
+    //Prints my hobby
+    printf("My hobby is playing a computer\n");
 }
